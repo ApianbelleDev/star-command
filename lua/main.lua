45839ab1b82726140ccca39c8d502aab14b6b791
ApianbelleDev <miracleforcemanga@gmail.com>
2023-09-09 12:39:02 -0400

    added pause logic and comet graphics

--- lua/main.lua
+++ lua/main.lua
@@ -4,13 +4,19 @@ local screenScale
 function love.load()
 
 	-- setup tables for objects
-	ship = {}
+	game   = {}
+	ship   = {}
+	bullet = {}
+	comet  = {}
+
+	game.isPaused = false
+	game.gameOver = false
+	game.text = "nothing" -- debug text
+	
 	ship.image = love.graphics.newImage("res/ship.png", nil)
 	ship.x = 80 - 16
 	ship.y = 144 - 16
-
-	bullet = {}
-	-- bullet.image = love.graphics.newImage("res/bullet.png", nil)
+	ship.speed = 5
 
 	-- store bullet off screen
 	bullet.x = -100
@@ -18,13 +24,19 @@ function love.load()
 	bullet.isShot = false
 	bullet.count = 1
 
+	-- store comet off screen **away** from bullet so that collision doesn't 
+	comet.image = love.graphics.newImage("res/comet.png", nil)
+	comet.x = -200
+	comet.y = -200
+	comet.speed = 3
+	
 	-- override default filter so game when scaled up doesn't look blurry
-	love.graphics.setDefaultFilter("nearest", "nearest", 0)
+	love.graphics.setDefaultFilter("nearest", nil, 0)
 
-	sx = 160;
-	sy = 144;
+	sx = 160
+	sy = 144
 
-	screenScale = 4.0;
+	screenScale = 4.0
 	-- create canvas to draw to that will be scaled later
 	canvas = love.graphics.newCanvas(sx, sy)
 
@@ -32,12 +44,22 @@ function love.load()
 	love.window.setMode(sx * 4, sy * 4)
 end
 
-function love.update()
+function love.keypressed( key )
+	-- pause logic
+	if not game.isPaused and key == "return" then
+		game.isPaused = true
+	elseif game.isPaused and key == "return" then
+		game.isPaused = false
+	end
+end
+
+
+function update()
 	-- ship left/right movement
 	if love.keyboard.isDown("left") then
 		ship.x = ship.x - 1
 	end
-
+	
 	if love.keyboard.isDown("right") then
 		ship.x = ship.x + 1
 	end
@@ -64,7 +86,6 @@ function love.update()
 			bullet.isShot = true
 		end
 	end
-
 	-- destroy bullet if it reaches the top of the screen
 	if bullet.y == 0 then
 		bullet.isShot = false
@@ -72,7 +93,12 @@ function love.update()
 		bullet.x = -100
 		bullet.y = -100
 	end
+end
 
+function love.update()
+	if not game.isPaused then
+		update()
+	end
 end
 
 function love.draw()
@@ -81,14 +107,16 @@ function love.draw()
 	love.graphics.clear();
 
 	-- draw to the canvas
-	love.graphics.draw(ship.image, ship.x, ship.y);
-	love.graphics.rectangle("fill", bullet.x, bullet.y, 4, 4);
+	love.graphics.draw(ship.image, ship.x, ship.y)
+	love.graphics.rectangle("fill", bullet.x, bullet.y, 4, 4)
 
 	-- go back to drawing on screen
 	love.graphics.setCanvas()
-	love.graphics.print("ship x = "..tostring(ship.x));
-	love.graphics.print("bulletCount = "..tostring(bullet.count), 0, 16);
-	love.graphics.print("isShot = "..tostring(bullet.isShot), 0, 32);
+	love.graphics.print("ship x = "..tostring(ship.x))
+	love.graphics.print("bulletCount = "..tostring(bullet.count), 0, 16)
+	love.graphics.print("isShot = "..tostring(bullet.isShot), 0, 32)
+	love.graphics.print("isPaused = "..tostring(game.isPaused), 0, 48)
+	love.graphics.print("text = "..game.text, 0, 64)
 
 	-- render canvas on screen
 	love.graphics.draw(canvas, 0, 0, 0, screenScale)	
